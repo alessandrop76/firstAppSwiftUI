19a84f70e8694f55aa071dc3deed11d89d7cc509
alessandrop76 <alessandrodigima@gmail.com>
2023-08-04 19:38:53 +0100

    Initial Commit

---
+++ firstAppSwiftUI/firstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppSwiftUIApp.swift
+//  firstAppSwiftUI
+//
+//  Created by alessandro pereira on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
